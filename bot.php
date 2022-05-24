73e8b3710ce89c3c5cae9f91583464c994a439e1
r3cruit <105633417+r3cruit@users.noreply.github.com>
2022-05-24 10:00:56 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,79 +1,40 @@
-import telebot
-import bs4
-from Task import Task
-import parser
+<?php
+    include('vendor/autoload.php'); //Подключаем библиотеку
+    use Telegram\Bot\Api; 
 
-#main variables
-TOKEN = '5333895852:AAH2Ht-Mw1ituSvoKfGPlD4xWkwcF2RrJ-E'
-bot = telebot.TeleBot(TOKEN)
-task = Task()
+    $telegram = new Api('5333895852:AAH2Ht-Mw1ituSvoKfGPlD4xWkwcF2RrJ-E'); //Устанавливаем токен, полученный у BotFather
+    $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
+    
+    $text = $result["message"]["text"]; //Текст сообщения
+    $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
+    $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
+    $keyboard = [["Последние статьи"],["Картинка"],["Гифка"]]; //Клавиатура
 
-#handlers
-@bot.message_handler(commands=['start', 'go'])
-def start_handler(message):
-    if not task.isRunning:
-        chat_id = message.chat.id
-        msg = bot.send_message(chat_id, 'Откуда парсить?')
-        bot.register_next_step_handler(msg, askSource)
-        task.isRunning = True
-
-def askSource(message):
-    chat_id = message.chat.id
-    text = message.text.lower()
-    if text in task.names[0]:
-        task.mySource = 'top'
-        msg = bot.send_message(chat_id, 'За какой временной промежуток?')
-        bot.register_next_step_handler(msg, askAge)
-    elif text in task.names[1]:
-        task.mySource = 'all'
-        msg = bot.send_message(chat_id, 'Какой минимальный порог рейтинга?')
-        bot.register_next_step_handler(msg, askRating)
-    else:
-        msg = bot.send_message(chat_id, 'Такого раздела нет. Введите раздел корректно.')
-        bot.register_next_step_handler(msg, askSource)
-        return
-
-def askAge(message):
-    chat_id = message.chat.id
-    text = message.text.lower()
-    filters = task.filters[0]
-    if text not in filters:
-        msg = bot.send_message(chat_id, 'Такого временного промежутка нет. Введите порог корректно.')
-        bot.register_next_step_handler(msg, askAge)
-        return
-    task.myFilter = task.filters_code_names[0][filters.index(text)]
-    msg = bot.send_message(chat_id, 'Сколько страниц парсить?')
-    bot.register_next_step_handler(msg, askAmount)
-
-def askRating(message):
-    chat_id = message.chat.id
-    text = message.text.lower()
-    filters = task.filters[1]
-    if text not in filters:
-        msg = bot.send_message(chat_id, 'Такого порога нет. Введите порог корректно.')
-        bot.register_next_step_handler(msg, askRating)
-        return
-    task.myFilter = task.filters_code_names[1][filters.index(text)]
-    msg = bot.send_message(chat_id, 'Сколько страниц парсить?')
-    bot.register_next_step_handler(msg, askAmount)
-
-def askAmount(message):
-    chat_id = message.chat.id
-    text = message.text.lower()
-    if not text.isdigit():
-        msg = bot.send_message(chat_id, 'Количество страниц должно быть числом. Введите корректно.')
-        bot.register_next_step_handler(msg, askAmount)
-        return
-    if int(text) < 1 or int(text) > 11:
-        msg = bot.send_message(chat_id, 'Количество страниц должно быть >0 и <11. Введите корректно.')
-        bot.register_next_step_handler(msg, askAmount)
-        return
-    task.isRunning = False
-    output = ''
-    if task.mySource == 'top':
-        output = parser.getTitlesFromTop(int(text), task.myFilter)
-    else:
-        output = parser.getTitlesFromAll(int(text), task.myFilter)
-    msg = bot.send_message(chat_id, output)
-
-bot.polling(none_stop=True)
+    if($text){
+         if ($text == "/start") {
+            $reply = "Добро пожаловать в бота!";
+            $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+        }elseif ($text == "/help") {
+            $reply = "Информация с помощью.";
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+        }elseif ($text == "Картинка") {
+            $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
+            $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
+        }elseif ($text == "Гифка") {
+            $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
+            $telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
+        }elseif ($text == "Последние статьи") {
+            $html=simplexml_load_file('http://netology.ru/blog/rss.xml');
+            foreach ($html->channel->item as $item) {
+	     $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
+        	}
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
+        }else{
+        	$reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
+        	$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
+        }
+    }else{
+    	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
+    }
+?>
